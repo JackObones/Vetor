62620ad189c6992edf26c3e89435ebe03cefb4f4
Gustavo <54158383+JackObones@users.noreply.github.com>
2020-06-30 20:36:53 -0300

    Add files via upload

---
+++ Vetor1/Cadastro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Vetor1
+{
+    class Cadastro
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        
+        public Cadastro(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
